eac908443bf43fd4bb0c4d18117dbf5a5e0859e1
Vikash21s <mrvikashsharma75@gmail.com>
2024-09-03 01:04:46 +0530

    initial commit

--- chatwing/lib/Pages/Chat/chatpage.dart
+++ chatwing/lib/Pages/Chat/chatpage.dart
@@ -1,3 +1,4 @@
+import 'package:chatwing/Config/images.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
@@ -7,16 +8,34 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Image.asset(AssetsImage.boyPic),
+        ),
         title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Deepak Kumar"),
+            Text("Deepak", style: Theme.of(context).textTheme.bodyLarge),
             Text(
               "online",
               style: Theme.of(context).textTheme.labelSmall,
             )
           ],
         ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.phone,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.video_call,
+            ),
+          )
+        ],
       ),
       body: Column(
         children: [],
