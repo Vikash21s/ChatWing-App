fee1ed2367abc3fd7e406ab1173a9cf01d634ce9
Vikash21s <mrvikashsharma75@gmail.com>
2024-09-03 00:06:53 +0530

    initiall commit

--- chatwing/lib/Pages/HomePage/Widget/chatlist.dart
+++ chatwing/lib/Pages/HomePage/Widget/chatlist.dart
@@ -1,4 +1,5 @@
 import 'package:chatwing/Config/images.dart';
+import 'package:chatwing/Pages/HomePage/Widget/chattile.dart';
 import 'package:flutter/material.dart';
 
 class ChatList extends StatelessWidget {
@@ -8,45 +9,8 @@ class ChatList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        Container(
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: Theme.of(context).colorScheme.primaryContainer,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Image.asset(
-                    AssetsImage.girlPic,
-                    width: 70,
-                  ),
-                  SizedBox(width: 15),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Deepak",
-                        style: Theme.of(context).textTheme.bodyLarge,
-                      ),
-                      SizedBox(height: 2), // we can change both text distance
-                      Text(
-                        "Baad Me bat krte hain",
-                        style: Theme.of(context).textTheme.labelMedium,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Text(
-                "08:33 PM",
-                style: Theme.of(context).textTheme.labelMedium,
-              ),
-            ],
-          ),
-        )
+        ChatTile(),
+        ChatTile(),
       ],
     );
   }
---
+++ chatwing/lib/Pages/HomePage/Widget/chattile.dart
@@ -0,0 +1,49 @@
+import 'package:chatwing/Config/images.dart';
+import 'package:flutter/material.dart';
+
+class ChatTile extends StatelessWidget {
+  const ChatTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Theme.of(context).colorScheme.primaryContainer,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Image.asset(
+                AssetsImage.girlPic,
+                width: 70,
+              ),
+              SizedBox(width: 15),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Deepak",
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  SizedBox(height: 2), // we can change both text distance
+                  Text(
+                    "Baad Me bat krte hain",
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Text(
+            "08:33 PM",
+            style: Theme.of(context).textTheme.labelMedium,
+          ),
+        ],
+      ),
+    );
+  }
+}
