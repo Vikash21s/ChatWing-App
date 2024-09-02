061e67b252e1cbd7cf65f3fdc40b62fa406b0bdf
Vikash21s <mrvikashsharma75@gmail.com>
2024-09-03 00:01:28 +0530

    initial commit

--- chatwing/lib/Pages/HomePage/Widget/chatlist.dart
+++ chatwing/lib/Pages/HomePage/Widget/chatlist.dart
@@ -9,26 +9,40 @@ class ChatList extends StatelessWidget {
     return ListView(
       children: [
         Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Theme.of(context).colorScheme.primaryContainer,
+          ),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Image.asset(
-                AssetsImage.girlPic,
-                width: 70,
-              ),
-              SizedBox(width: 15),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Row(
                 children: [
-                  Text(
-                    "Deepak",
-                    style: Theme.of(context).textTheme.bodyLarge,
+                  Image.asset(
+                    AssetsImage.girlPic,
+                    width: 70,
                   ),
-                  SizedBox(height: 2), // we can change both text distance
-                  Text(
-                    "Baad Me bat krte hain",
-                    style: Theme.of(context).textTheme.labelMedium,
+                  SizedBox(width: 15),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Deepak",
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      SizedBox(height: 2), // we can change both text distance
+                      Text(
+                        "Baad Me bat krte hain",
+                        style: Theme.of(context).textTheme.labelMedium,
+                      ),
+                    ],
                   ),
                 ],
+              ),
+              Text(
+                "08:33 PM",
+                style: Theme.of(context).textTheme.labelMedium,
               ),
             ],
           ),
