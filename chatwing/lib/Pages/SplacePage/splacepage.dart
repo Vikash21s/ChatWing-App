bdd62367b4c0a868f48ea2b6134e336eda4460cb
Vikash21s <mrvikashsharma75@gmail.com>
2024-09-06 20:17:00 +0530

    splace page

---
+++ chatwing/lib/Pages/SplacePage/splacepage.dart
@@ -0,0 +1,22 @@
+import 'package:chatwing/Config/images.dart';
+import 'package:chatwing/Controller/splacecontroller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+class Splacepage extends StatelessWidget {
+  const Splacepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    splacecontroller splaceController = Get.put(splacecontroller());
+    return Scaffold(
+      body: Center(
+        child: SvgPicture.asset(
+          AssetsImage.appIconSVG,
+        ),
+      ),
+    );
+  }
+}
