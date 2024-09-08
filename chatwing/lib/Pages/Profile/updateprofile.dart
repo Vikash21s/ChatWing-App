22932db607dd979da76d29cad9dd5e0f4ee48624
Vikash21s <mrvikashsharma75@gmail.com>
2024-09-08 13:05:27 +0530

    update profile semi page

--- chatwing/lib/Pages/Profile/updateprofile.dart
+++ chatwing/lib/Pages/Profile/updateprofile.dart
@@ -9,8 +9,94 @@ class UpdateProfile extends StatelessWidget {
       appBar: AppBar(
         title: Text("Update Profile"),
       ),
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Theme.of(context).colorScheme.primaryContainer),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Column(
+                    children: [
+                      Container(
+                        width: 200,
+                        height: 200,
+                        child: Center(
+                          child: Icon(
+                            Icons.image,
+                            size: 40,
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context)
+                              .colorScheme
+                              .background, //yha se image ya dp ka backgroun ka color change kr skte hain
+                          borderRadius: BorderRadius.circular(
+                            100,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Text(
+                            "Personal Info",
+                            style: Theme.of(context).textTheme.labelMedium,
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Text(
+                            "Name",
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
+                        textInputAction: TextInputAction.next,
+                        decoration: InputDecoration(
+                          hintText: "Enter Your Name",
+                          hintStyle: Theme.of(context).textTheme.labelLarge,
+                          prefixIcon: Icon(
+                            Icons.person,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Text(
+                            "Email id",
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
+                        textInputAction: TextInputAction.next,
+                        decoration: InputDecoration(
+                          hintText: "Enter Your Email",
+                          hintStyle: Theme.of(context).textTheme.labelLarge,
+                          prefixIcon: Icon(
+                            Icons.alternate_email_rounded,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ))
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
