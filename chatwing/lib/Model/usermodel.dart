e841351a8f22a557bb39984484494fd70261dffd
Vikash21s <mrvikashsharma75@gmail.com>
2024-09-12 19:16:26 +0530

    profile name get

--- chatwing/lib/Model/usermodel.dart
+++ chatwing/lib/Model/usermodel.dart
@@ -4,9 +4,24 @@ class UserModel {
   String? email;
   String? profileImage;
   String? phoneNumber;
+  String? about;
+  String? createdAt;
+  String? lastOnlineStatus;
+  String? status;
+  String? role;
 
-  UserModel(
-      {this.id, this.name, this.email, this.profileImage, this.phoneNumber});
+  UserModel({
+    this.id,
+    this.name,
+    this.email,
+    this.profileImage,
+    this.phoneNumber,
+    this.about,
+    this.createdAt,
+    this.lastOnlineStatus,
+    this.status,
+    this.role,
+  });
 
   UserModel.fromJson(Map<String, dynamic> json) {
     id = json["id"];
@@ -14,6 +29,11 @@ class UserModel {
     email = json["email"];
     profileImage = json["profileImage"];
     phoneNumber = json["phoneNumber"];
+    about = json["About"];
+    createdAt = json["CreatedAt"];
+    lastOnlineStatus = json["LastOnlineStatus"];
+    status = json["Status"];
+    role = json["role"];
   }
 
   Map<String, dynamic> toJson() {
@@ -23,6 +43,11 @@ class UserModel {
     _data["email"] = email;
     _data["profileImage"] = profileImage;
     _data["phoneNumber"] = phoneNumber;
+    _data["About"] = about;
+    _data["CreatedAt"] = createdAt;
+    _data["LastOnlineStatus"] = lastOnlineStatus;
+    _data["Status"] = status;
+    _data["role"] = role;
     return _data;
   }
 }
