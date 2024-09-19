77c5fac70e9d968b93ed01a872636444b9c1f219
Vikash21s <mrvikashsharma75@gmail.com>
2024-09-20 01:51:15 +0530

    use cache for image in send message

--- chatwing/lib/Pages/Chat/Widgets/chatbubble.dart
+++ chatwing/lib/Pages/Chat/Widgets/chatbubble.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chatwing/Config/images.dart';
+import 'package:chatwing/Controller/profilecontroller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -26,38 +28,46 @@ class ChatBubble extends StatelessWidget {
             isComming ? CrossAxisAlignment.start : CrossAxisAlignment.end,
         children: [
           Container(
-              padding: EdgeInsets.all(10),
-              constraints: BoxConstraints(
-                  //minWidth: 100,
-                  maxWidth: MediaQuery.sizeOf(context).width / 1.3),
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primaryContainer,
-                borderRadius: isComming
-                    ? BorderRadius.only(
-                        topLeft: Radius.circular(0), // we can change text box
-                        topRight: Radius.circular(15),
-                        bottomLeft: Radius.circular(17),
-                        bottomRight: Radius.circular(15),
-                      )
-                    : BorderRadius.only(
-                        topLeft: Radius.circular(15), // we can change text box
-                        topRight: Radius.circular(15),
-                        bottomLeft: Radius.circular(17),
-                        bottomRight: Radius.circular(0),
-                      ),
-              ),
-              child: imageUrl == ""
-                  ? Text(message)
-                  : Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.network(imageUrl)),
-                        SizedBox(height: 10),
-                        Text(message),
-                      ],
-                    )),
+            padding: EdgeInsets.all(10),
+            constraints: BoxConstraints(
+                //minWidth: 100,
+                maxWidth: MediaQuery.sizeOf(context).width / 1.3),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              borderRadius: isComming
+                  ? BorderRadius.only(
+                      topLeft: Radius.circular(0), // we can change text box
+                      topRight: Radius.circular(15),
+                      bottomLeft: Radius.circular(17),
+                      bottomRight: Radius.circular(15),
+                    )
+                  : BorderRadius.only(
+                      topLeft: Radius.circular(15), // we can change text box
+                      topRight: Radius.circular(15),
+                      bottomLeft: Radius.circular(17),
+                      bottomRight: Radius.circular(0),
+                    ),
+            ),
+            child: imageUrl == ""
+                ? Text(message)
+                : Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: CachedNetworkImage(
+                            imageUrl: imageUrl,
+                            fit: BoxFit.cover,
+                            placeholder: (context, url) =>
+                                CircularProgressIndicator(),
+                            errorWidget: (context, url, error) =>
+                                Icon(Icons.error),
+                          )),
+                      message == "" ? Container() : SizedBox(height: 10),
+                      message == "" ? Container() : Text(message),
+                    ],
+                  ),
+          ),
           SizedBox(height: 10),
           Row(
             mainAxisAlignment:
