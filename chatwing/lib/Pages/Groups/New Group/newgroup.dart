441e47d0a482d3a0f59bdf140d04a7b42f4f91d0
Vikash21s <mrvikashsharma75@gmail.com>
2024-09-20 18:48:18 +0530

    Right icon add

--- chatwing/lib/Pages/Groups/New Group/newgroup.dart
+++ chatwing/lib/Pages/Groups/New Group/newgroup.dart
@@ -11,37 +11,47 @@ class NewGroup extends StatelessWidget {
   Widget build(BuildContext context) {
     ContactController contactController = Get.put(ContactController());
     return Scaffold(
-      body: StreamBuilder(
-        stream: contactController.getContacts(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (snapshot.hasError) {
-            return Center(
-              child: Text("Error: ${snapshot.error}"),
-            );
-          }
-          if (snapshot.data == null) {
-            return const Center(
-              child: Text("No Messages"),
-            );
-          } else {
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                return ChatTile(
-                    imageUrl: snapshot.data![index].profileImage ??
-                        AssetsImage.defaultProfileUrl,
-                    name: snapshot.data![index].name!,
-                    lastChat: snapshot.data![index].about ?? "",
-                    lastTime: "lastTime");
-              },
-            );
-          }
-        },
+      appBar: AppBar(
+        title: Text('New Group'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.arrow_forward),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: StreamBuilder(
+          stream: contactController.getContacts(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.hasError) {
+              return Center(
+                child: Text("Error: ${snapshot.error}"),
+              );
+            }
+            if (snapshot.data == null) {
+              return const Center(
+                child: Text("No Messages"),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return ChatTile(
+                      imageUrl: snapshot.data![index].profileImage ??
+                          AssetsImage.defaultProfileUrl,
+                      name: snapshot.data![index].name!,
+                      lastChat: snapshot.data![index].about ?? "",
+                      lastTime: "lastTime");
+                },
+              );
+            }
+          },
+        ),
       ),
     );
   }
