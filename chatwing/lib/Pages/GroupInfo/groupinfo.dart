9e0cb847614504183da1ec6de507fa1425c7b759
Vikash21s <mrvikashsharma75@gmail.com>
2024-09-22 02:54:53 +0530

    Profile edit in group

--- chatwing/lib/Pages/GroupInfo/groupinfo.dart
+++ chatwing/lib/Pages/GroupInfo/groupinfo.dart
@@ -1,4 +1,6 @@
+import 'package:chatwing/Config/images.dart';
 import 'package:chatwing/Model/groupmodel.dart';
+import 'package:chatwing/Pages/UserProfile/Widgets/userinfo.dart';
 import 'package:flutter/material.dart';
 
 class GroupInfo extends StatelessWidget {
@@ -20,7 +22,15 @@ class GroupInfo extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(10),
         child: ListView(
-          children: [],
+          children: [
+            LoginUserInfo(
+              profileImage: groupModel.profileUrl == ""
+                  ? AssetsImage.defaultProfileUrl
+                  : groupModel.profileUrl!,
+              userName: groupModel.name!,
+              userEmail: groupModel.description ?? "No Description Available",
+            )
+          ],
         ),
       ),
     );
