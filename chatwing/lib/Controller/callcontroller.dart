a2456942c94a52158c6836e1e16fcf3d82fe0e9a
Vikash21s <mrvikashsharma75@gmail.com>
2024-09-23 01:50:26 +0530

    notification perfect work

--- chatwing/lib/Controller/callcontroller.dart
+++ chatwing/lib/Controller/callcontroller.dart
@@ -2,6 +2,8 @@ import 'package:chatwing/Model/audiocall.dart';
 import 'package:chatwing/Model/usermodel.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:uuid/uuid.dart';
 
@@ -9,6 +11,15 @@ class CallController extends GetxController {
   final db = FirebaseFirestore.instance;
   final auth = FirebaseAuth.instance;
   final uuid = Uuid().v6();
+
+  void onInit() {
+    super.onInit();
+    getCallsNotification().listen((snapshot) {
+      if (snapshot.docs.isNotEmpty) {
+        Get.snackbar("Calling", "Calling");
+      }
+    });
+  }
 
   Future<void> callAction(UserModel receiver, UserModel caller) async {
     String id = uuid;
@@ -28,7 +39,7 @@ class CallController extends GetxController {
     try {
       await db
           .collection("notification")
-          .doc(auth.currentUser!.uid) // yha uske me doc(receiver.id)   hai..
+          .doc(receiver.id) // yha uske me doc(receiver.id)   hai..
           .collection("call")
           .doc(id) //
           .set(newCall.toJson());
@@ -37,5 +48,13 @@ class CallController extends GetxController {
       print(
           "🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥");
     }
+  }
+
+  Stream<QuerySnapshot> getCallsNotification() {
+    return db
+        .collection("notification")
+        .doc(auth.currentUser!.uid)
+        .collection("call")
+        .snapshots();
   }
 }
--- chatwing/lib/main.dart
+++ chatwing/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:chatwing/Config/pagepath.dart';
 import 'package:chatwing/Config/theme.dart';
+import 'package:chatwing/Controller/callcontroller.dart';
 import 'package:chatwing/Pages/Auth/authpage.dart';
 import 'package:chatwing/Pages/HomePage/homepage.dart';
 import 'package:chatwing/Pages/SplacePage/splacepage.dart';
@@ -8,6 +9,7 @@ import 'package:chatwing/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() async {
@@ -26,6 +28,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    CallController callController = Get.put(CallController());
     return GetMaterialApp(
       builder: FToastBuilder(),
       title: 'Flutter Demo',
